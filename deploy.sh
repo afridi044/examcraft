e958d33df551586cbefc1e20c9d8864c7bef2f11
SajidNoor5051 <sajidmostafiznoor@gmail.com>
2025-07-07 11:38:45 +0600

    cd fix

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+echo "🚀Starting deployment script..."
+
+
+cd /opt/examcraft
+
+git pull origin master || echo "No changes to pull"
+
+# Copy environment file
+cp /home/seed/env_folder/.env.local /opt/examcraft/backend/.env|| echo "No .env.example found, using existing .env"
+
+
+echo "dcoker compose backend"
+
+cd /opt/examcraft/backend
+
+docker-compose -f docker-compose.yml down
+docker-compose -f docker-compose.yml up --build -d
+
+echo "Waiting for backend to be ready..."
+sleep 30
+
+echo "dcoker compose frontend"
+
+cd /opt/examcraft/frontend
+docker-compose -f docker-compose.yml down
+docker-compose -f docker-compose.yml up --build -d
+
+docker ps -a
+
+echo "Deployment completed successfully! 🎉"
+
+
